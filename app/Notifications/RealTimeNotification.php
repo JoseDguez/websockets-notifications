2e4ca85a39ca028fd7168ac14b06777b411f5d35
Jose Antonio Dominguez <joseantonio.dguez@gmail.com>
2021-12-16 21:23:04 -0800

    First commit

---
+++ app/Notifications/RealTimeNotification.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\BroadcastMessage;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class RealTimeNotification extends Notification implements ShouldBroadcast
+{
+    use Queueable;
+
+    public string $message;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(string $message)
+    {
+        $this->message = $message;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['broadcast'];
+    }
+
+    public function toBroadcast($notifiable): BroadcastMessage
+    {
+        return new BroadcastMessage([
+            'message' => "$this->message (User ID: $notifiable->id)"
+        ]);
+    }
+}
